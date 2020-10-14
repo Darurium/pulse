4fd475129d64c7ffb9d331d4e6c9a118ea17f27f
Alex <darurium@gmail.com>
2020-10-15 02:36:37 +0500

    Add tabs js

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,6 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/arrow-left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/arrow-right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 1024,
-//                 settings: {
-//                   slidesToShow: 3,
-//                   slidesToScroll: 3,
-//                   infinite: true,
-//                   dots: true
-//                 }
-//             }
-//             ]
-//         });
+$(document).ready(function(){
 
-// });
-
-const slider = tns({
+  const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -41,3 +22,12 @@ const slider = tns({
   document.querySelector('.prev').addEventListener('click', function() {
       slider.goTo('prev') 
     });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+});
+
