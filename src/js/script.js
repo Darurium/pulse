f4f96963b6b1e47c361c2bb45cafb19903997edf
Alex <darurium@gmail.com>
2020-10-25 20:55:20 +0500

    page up

--- src/js/script.js
+++ src/js/script.js
@@ -104,4 +104,24 @@ $('form').submit(function(e) {
 	return false;
 });
 
+// scroll page up
+
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 1600 ) {
+		$('.page-up').fadeIn();
+	}
+	else {
+		$('.page-up').fadeOut();
+	}
 })
+
+// Плавный скрол
+
+$("a[href^='#']").click(function(){
+	const _href = $(this).attr("href");
+	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+	return false;
+});
+
+
+});
